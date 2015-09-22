35bce4baa80fa9e872d0a0c897b939382771ad8a
Vinoth <a.vinothk@outlook.com>
2015-09-22 19:44:43 +0530

    Move to react, Redesign the page

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var Forms = React.createClass({
+  render: function() {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 inputs">
+            <h1 className="text-center headtext">Find number of times a word is said in a movie</h1>
+            <input type="text" className="form-control element" id="movie" placeholder="Type any movie name here..."/>
+            <input type="text" className="form-control element" id="word"  placeholder="Type the word here..."/>
+            <button type="button" className="btn btn-md btn-info element" id="buton">Go!</button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <Forms />,
+  document.getElementById('forms')
+);
+
